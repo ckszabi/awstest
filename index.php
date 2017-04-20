75d7177756550817c1149357cc74d82bf825d59e
Szekely Szabolcs <szekely.szabolcs@redrock.no>
2017-04-20 14:15:21 +0300

    requiring config

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
- echo "php deployed";
+ echo "php deployed<br>";
+
+ include './config/config.php';
+
+ if (isset($config)) {
+	 echo '$config is:<br>';
+	 var_dump($config);
+ }
+ else {
+	 echo '$config is not set';
+ }
+ 
